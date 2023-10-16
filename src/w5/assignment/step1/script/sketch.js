67c375ee179fa427ffde313f2dba734cb1716c25
“limjieun11” <1129127080+limjieun11@users.noreply.github.com>
2023-10-16 11:16:19 +0900

    1016과제5주차1

---
+++ src/w5/assignment/step1/script/sketch.js
@@ -0,0 +1,48 @@
+const cNum = 8;
+const rNum = 8;
+let gridC;
+let gridR;
+let angleBegin = 0;
+let angleBeginVel = 1;
+let angleStep = 15;
+
+function setup() {
+  setCanvasContainer('canvas', 1, 1, true);
+  colorMode(HSL, 360, 100, 100, 100);
+  background(360, 0, 100);
+
+  const margin = min(width, height) / 150;
+
+  gridC = (width - margin) / cNum;
+  gridR = (height - margin) / rNum;
+}
+
+function draw() {
+  background(360, 0, 100);
+
+  for (let a = 0; a < rNum; a++) {
+    for (let b = 0; b < cNum; b++) {
+      push();
+
+      const x = gridC * b + gridC / 2;
+      const y = gridR * a + gridR / 2;
+
+      translate(x, y);
+      rotate(radians(angleBegin + b * angleStep));
+
+      const colorIndex = (b + a) % 4;
+      stroke(90 * colorIndex, 80, 70);
+
+      line(0, 0, gridC / 4, 0);
+      noFill();
+      ellipse(0, 0, 50);
+
+      fill(0);
+      noStroke();
+      const circleSize = 15;
+      ellipse(gridC / 5, 0, circleSize);
+      pop();
+    }
+  }
+  angleBegin += angleBeginVel; // 각도 증가
+}
