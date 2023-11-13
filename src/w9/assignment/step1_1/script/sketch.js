f6c9878aae9d8a9b11151f18681f0c197d4c237e
“limjieun11” <1129127080+limjieun11@users.noreply.github.com>
2023-11-13 23:15:20 +0900

    1113

--- src/w9/assignment/step1_1/script/sketch.js
+++ src/w9/assignment/step1_1/script/sketch.js
@@ -12,12 +12,14 @@ const {
   Bodies,
 } = Matter;
 
+Common.setDecomp(decomp);
+
 // create engine
-var engine = Engine.create(),
+const engine = Engine.create(),
   world = engine.world;
 
 // 러너 만들고 실행하기
-var runner = Runner.create();
+const runner = Runner.create();
 Runner.run(runner, engine);
 
 let group;
@@ -29,11 +31,14 @@ let vertices = [];
 let m;
 let mc;
 
-const originalWidth = 800;
+const oWidth = 800;
 const originalHeight = 600;
 
+let released = false;
+
 function setup() {
-  setCanvasContainer('canvas', originalWidth, originalHeight, true);
+  setCanvasContainer('canvas', 800, 600, true);
+
   rectMode(CENTER);
 
   // add bodies
@@ -41,8 +46,8 @@ function setup() {
   group = Body.nextGroup(true);
 
   ropeA = Composites.stack(100, 50, 8, 1, 10, 10, function (x, y) {
-    // vertices = Vertices.fromPath('10 30 50 0 50 20 0 20');
-    return Bodies.rectangle(x, y, 50, 20, {
+    vertices = Vertices.fromPath('10 30 50 0 50 20 0 20');
+    return Bodies.fromVertices(x, y, vertices, {
       collisionFilter: { group: group },
     });
   });
@@ -50,7 +55,7 @@ function setup() {
   Composites.chain(ropeA, 0.5, 0, -0.5, 0, {
     stiffness: 0.8,
     length: 2,
-    render: { type: 'line' },
+    render: { fillStyle: '#060a19' },
   });
   Composite.add(
     ropeA,
@@ -88,7 +93,7 @@ function setup() {
   group = Body.nextGroup(true);
 
   ropeC = Composites.stack(600, 50, 13, 1, 10, 10, function (x, y) {
-    // vertices = Vertices.fromPath('10 30 50 0 50 20 0 20');
+    vertices = Vertices.fromPath('10 30 50 0 50 20 0 20');
     return Bodies.rectangle(x - 20, y, 50, 20, {
       collisionFilter: { group: group },
       chamfer: 5,
@@ -115,7 +120,7 @@ function setup() {
   ]);
 
   m = Mouse.create(document.querySelector('.p5Canvas'));
-  m.pixelRatio = (pixelDensity() * width) / originalWidth;
+  m.pixelRatio = (pixelDensity() * width) / oWidth;
   mc = MouseConstraint.create(engine, {
     mouse: m,
     Constraint: {
@@ -134,44 +139,36 @@ function setup() {
 }
 
 function draw() {
-  m.pixelRatio = (pixelDensity() * width) / originalWidth;
+  m.pixelRatio = (pixelDensity() * width) / oWidth;
   background('white');
+  for (let rope of [ropeA, ropeB, ropeC]) {
+    fill(0); // 도형의 색을 검정색으로 설정
 
-  for (let composite of world.composites) {
-    for (let body of composite.bodies) {
-      // 무작위 색상 설정
-      fill('blue');
-      noStroke();
-
-      // 각 도형에 원하는 색상을 채워 그립니다.
+    // 로프의 각 도형에 대해 반복
+    for (let body of rope.bodies) {
       beginShape();
-      for (let vert of body.vertices) {
-        vertex(vert.x, vert.y);
+      // 각 도형의 꼭짓점을 추가
+      for (let vertex of body.vertices) {
+        vertex(
+          (vertex.x / originalWidth) * width,
+          (vertex.y / originalHeight) * height
+        );
       }
       endShape(CLOSE);
     }
   }
-
-  for (let composite of world.composites) {
-    for (let body of composite.bodies) {
-      // 무작위 색상 설정
-      fill('skyblue');
-      noStroke();
-
-      // 각 도형에 원하는 색상을 채워 그립니다.
-      beginShape();
-      for (let vert of body.vertices) {
-        vertex(vert.x, vert.y);
-      }
-      endShape(CLOSE);
-    }
-  }
-
+  // for (let i = 0; i < 10; i++) {
+  //   let angle = map(i, 0, 10, 0, TWO_PI);
+  //   let radius = random(20, 50);
+  //   let x = width / 2 + cos(angle) * radius;
+  //   let y = height / 2 + sin(angle) * radius;
+  //   vertices.push(createVector(x, y));
+  // }
   // fill(0);
   // beginShape();
   // ropeA.bodies.forEach((each) => {
   //   vertex(
-  //     (each.x / originalWidth) * width,
+  //     (each.x / oWidth) * width,
   //     (each.y / originalHeight) * height
   //   );
   // });
