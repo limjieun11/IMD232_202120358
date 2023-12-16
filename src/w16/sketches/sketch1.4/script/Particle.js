cc405168b38f6ead7f0280eaf85feb44aa2e0792
“limjieun11” <1129127080+limjieun11@users.noreply.github.com>
2023-12-17 01:40:15 +0900

    기말연습

---
+++ src/w16/sketches/sketch1.4/script/Particle.js
@@ -0,0 +1,31 @@
+class Particle {
+  constructor(x, y, tarX, tarY, col) {
+    this.pos = createVector(x, y);
+    this.targetPos = createVector(tarX, tarY);
+    this.color = col;
+    this.state = true;
+  }
+
+  update() {
+    // const dist = p5.Vector.sub(this.targetPos, this.pos);
+    // dist.mult(0.1);
+    // this.pos.add(dist);
+
+    const mouse = createVector(mouseX, mouseY);
+    const d = p5.Vector.sub(mouse, this.pos);
+    const distance = d.mag();
+    if (distance < 60) {
+      d.setMag(-4);
+      this.pos.add(d);
+    } else {
+      const dist = p5.Vector.sub(this.targetPos, this.pos);
+      dist.mult(0.1);
+      this.pos.add(dist);
+    }
+  }
+
+  display() {
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, 5);
+  }
+}
