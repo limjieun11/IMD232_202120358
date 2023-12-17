e809e4c87b69756142453d3eb6108571f030128e
“limjieun11” <1129127080+limjieun11@users.noreply.github.com>
2023-12-18 07:48:28 +0900

    기말 최종

--- src/w16/sketches/sketch1.4/script/Particle.js
+++ src/w16/sketches/sketch1.4/script/Particle.js
@@ -7,15 +7,11 @@ class Particle {
   }
 
   update() {
-    // const dist = p5.Vector.sub(this.targetPos, this.pos);
-    // dist.mult(0.1);
-    // this.pos.add(dist);
-
     const mouse = createVector(mouseX, mouseY);
     const d = p5.Vector.sub(mouse, this.pos);
     const distance = d.mag();
     if (distance < 70) {
-      d.setMag(-10);
+      d.setMag(-8);
       this.pos.add(d);
     } else {
       const dist = p5.Vector.sub(this.targetPos, this.pos);
